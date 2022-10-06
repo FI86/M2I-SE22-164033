fed914768b4b8524ca12cf99fa699aa67e64f4dd
FI86 <formation.informatique.86@gmail.com>
2022-10-06 11:44:34 +0200

    modif tkinter geometry

--- Corrigées/Base Python/tk_geometry_management.py
+++ Corrigées/Base Python/tk_geometry_management.py
@@ -15,10 +15,10 @@ label = ttk.Label(root, text = 'Hello, Tkinter!',background = 'green', anchor = 
 label.pack(fill = BOTH, expand = True)
 
 # Grid
-ttk.Label(root, text = 'Yellow', background = 'yellow').grid(row = 0, column = 0, stick=EW)
-ttk.Label(root, text = 'Blue', background = 'Blue').grid(row = 0, column = 1, stick=EW)
-ttk.Label(root, text = 'Green', background = 'Green').grid(row = 1, column = 0, stick=EW)
-ttk.Label(root, text = 'Orange', background = 'Orange').grid(row = 1, column = 1, stick=EW)
+ttk.Label(root, text = 'Yellow', background = 'yellow').grid(row = 0, column = 0, sticky=EW)
+ttk.Label(root, text = 'Blue', background = 'Blue').grid(row = 0, column = 1, sticky=EW)
+ttk.Label(root, text = 'Green', background = 'Green').grid(row = 1, column = 0, sticky=EW)
+ttk.Label(root, text = 'Orange', background = 'Orange').grid(row = 1, column = 1, sticky=EW)
 
 # Place
 # (Si besoin de la position exacte)
